8b40bccede0bf4d686fdfeec9f82fa64c1a27ce3
bhuvan <bhuvansompura2003@gmail.com>
2025-01-24 12:16:03 +0530

    add new data

--- Api/api/index.php
+++ Api/api/index.php
@@ -1,15 +1,52 @@
+<!-- basic api  -->
+
 <?php
 
 $url = "http://localhost/all-works/Api/api01/";
-// $url = "http://localhost/all-works/admin_03/allusers.php";
 $ch=curl_init();
 curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 $result=curl_exec($ch);
 curl_close($ch);
 $result = json_decode($result,true);
-echo '<pre>';
-print_r($result);
-
+// echo '<pre>';
+// print_r($result);
+if (isset($result['status'])) {
+    if ($result['status']==true) {
+        if (isset($result['result'])) {
+            if ($result['result']==true) {
+            //  echo '<pre>';
+            //  print_r($result['Data']);
+            ?>
+          <table>
+            <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Email</td>
+            </tr>
+            <?php
+            foreach ($result['Data'] as $list) {
+                echo 
+                "<tr>
+                 <td>".$list['id']."</td> 
+                <td>".$list['name']."</td>
+                <td>".$list['email']."</td>
+                
+                </tr>";
+            }
+            ?>
+          </table>
+            <?php
+            }else {
+                echo $result['Data'];
+            }
+        }
+    }else {
+        echo $result['Data'];
+        
+    }
+}else {
+    echo "Api not working";
+}
 
 ?>
